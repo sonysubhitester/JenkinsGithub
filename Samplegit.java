16da71ab3853d31adab754a4ed18a90e1ae4b980
sonysubhitester <sonysubhisony@gmail.com>
2024-09-15 19:20:46 +0530

    Create Samplegit.java

---
+++ Samplegit.java
@@ -0,0 +1,7 @@
+public class firstjenkins{
+  public static void main(String[] args){
+    System.out.println("hello one");
+    System.out.println("hello two");    
+    System.out.println("hello two");
+  }
+}
