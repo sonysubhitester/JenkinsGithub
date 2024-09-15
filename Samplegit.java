0fdc5c5bca3244588b23c288fef3cae8078dc923
sonysubhitester <sonysubhisony@gmail.com>
2024-09-15 19:22:49 +0530

    Update Samplegit.java

--- Samplegit.java
+++ Samplegit.java
@@ -2,6 +2,6 @@ public class firstjenkins{
   public static void main(String[] args){
     System.out.println("hello one");
     System.out.println("hello two");    
-    System.out.println("hello two");
+    System.out.println("hello three");
   }
 }
